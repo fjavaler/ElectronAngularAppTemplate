079c8d2a91f06453ddb7ff19d32560f902041bdd
Frederick Javalera <frederickjavalera@gmail.com>
2023-04-24 12:17:30 -0600

    Finished template.

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800
+  })
+
+  // Replace 'electron-with-angular-app-template' with name of app.
+  win.loadFile('dist/electron-with-angular-app-template/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
